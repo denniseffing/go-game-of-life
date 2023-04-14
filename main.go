6a8ef6b6d7401f3b5feca3b9cf6d4a575cd6b6a0
Dennis Effing <dennis.effing@outlook.com>
2023-04-15 01:29:06 +0200

    feat: play game of life as a glider

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+	"time"
+)
+
 func main() {
-	print("Hello world!")
+	slice := make([][]cell, 20)
+	for i := range slice {
+		slice[i] = make([]cell, 100)
+	}
+
+	slice[0][1].alive = true
+	slice[1][2].alive = true
+	slice[2][0].alive = true
+	slice[2][1].alive = true
+	slice[2][2].alive = true
+
+	world := world{slice}
+
+	for {
+		print(world.String())
+		world = world.age()
+		time.Sleep(100 * time.Millisecond)
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 }
